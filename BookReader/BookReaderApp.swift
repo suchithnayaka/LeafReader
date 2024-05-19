7602636f67f8dc2e2fcb9702d6a3fed77e742354
Suchith R N <suchithnayaka@gmail.com>
2024-05-19 11:53:45 +0530

    Initial Commit

---
+++ BookReader/BookReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookReaderApp.swift
+//  BookReader
+//
+//  Created by Suchith Nayaka on 19/05/24.
+//
+
+import SwiftUI
+
+@main
+struct BookReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
